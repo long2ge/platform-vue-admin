772456e42625e33ec4f1dfc42ffa0d1df57b6a0a
long2ge <444533452@qq.com>
2020-05-10 18:26:46 +0800

    del 去除多余代码

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -68,14 +68,14 @@ const actions = {
       logout(state.token).then(() => {
         // removeToken() // must remove  token  first
         commit('SET_TOKEN', '')
-        commit('SET_ROLES', [])
+        // commit('SET_ROLES', [])
         removeToken()
         resetRouter()
         
         // reset visited views and cached views
         // to fixed https://github.com/PanJiaChen/vue-element-admin/issues/2485
-        dispatch('tagsView/delAllViews', null, { root: true })
-        
+        // dispatch('tagsView/delAllViews', null, { root: true })
+
         resolve()
       }).catch(error => {
         reject(error)
